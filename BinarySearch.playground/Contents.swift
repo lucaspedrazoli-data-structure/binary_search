c558109f3f56422cf5e1e2c6a4e514cd97914cef
Lucas <lucaspedrazoli@gmail.com>
2020-09-30 17:59:48 -0300

    create binary search

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -1,3 +1,11 @@
 // Copyright (c) 2019 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+example(of: "binary search") {
+  let array = [1, 5, 15, 17, 19, 22, 24, 31, 105, 150]
+  let search31 = array.firstIndex(of: 31)
+  let binarySearch31 = array.binarySearch(for: 31)
+
+  print("first index of: \(String(describing: search31))")
+  print("binary search: \(String(describing: binarySearch31))")
+}
